94e72fc659a080785fef1ba238412abeb31652fc
Will Fong <fongwh1016@gmail.com>
2015-04-20 01:53:52 +0800

    Add the semihost syscall()
    The BKPT instruction for semihosting call

--- 06-Preemptive/semih.c
+++ 06-Preemptive/semih.c
@@ -1,20 +1,29 @@
 #include <string.h>
+#include <stdarg.h>
 #include "semih.h"
+
+int semihost_syscall(int num, ...)
+{
+	asm(
+		"BKPT 	0xAB\n"
+		"BX 	lr\n"
+		);
+}
 
 int open(const char *pathname, int flags)
 {
 	int argv[] = { (int) pathname, flags, strlen(pathname) };
-	return syscall(SYS_OPEN, argv);
+	return semihost_syscall(SYS_OPEN, argv);
 }
 
 int close(int fildes)
 {
-	return syscall(SYS_CLOSE, &fildes);
+	return semihost_syscall(SYS_CLOSE, &fildes);
 }
 
 int write(int fildes, const void *buf, int nbyte)
 {
 	int argv[] = { fildes, (int) buf, nbyte };
-	return nbyte - syscall(SYS_WRITE, argv);
+	return nbyte - semihost_syscall(SYS_WRITE, argv);
 }
 
--- 06-Preemptive/semih.h
+++ 06-Preemptive/semih.h
@@ -4,7 +4,7 @@
 #define SYS_CLOSE 0x02
 #define SYS_WRITE 0x05
 
-extern int syscall(int num, ...);
+int semihost_syscall(int number, ...) __attribute__((naked));
 
 int open(const char *pathname, int flags);
 int close(int fildes);
